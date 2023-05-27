31eebddc0d606bb38f3fa6d606f7edfa592ffdfa
gift4bd <101939620+gift4bd@users.noreply.github.com>
2023-05-28 03:23:13 +0600

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -26,7 +26,7 @@ def grab(url):
         #response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
+                print('https://raw.githubusercontent.com/gift4bd/yt_to_m3u/main/assets/moose_na.m3u')
                 return
             #os.system(f'wget {url} -O temp.txt')
             os.system(f'curl "{url}" > temp.txt')
