5f156fef46164d8742aea76e469d290543afbb8e
gift4bd <101939620+gift4bd@users.noreply.github.com>
2023-06-01 00:41:30 +0600

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -26,13 +26,13 @@ def grab(url):
         #response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://raw.githubusercontent.com/gift4bd/yt_to_m3u/main/assets/moose_na.m3u8')
+                print('https://raw.githubusercontent.com/gift4bd/yt-to-m3u/main/assets/moose_na.m3u8')
                 return
             #os.system(f'wget {url} -O temp.txt')
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/gift4bd/yt_to_m3u/main/assets/moose_na.m3u8')
+                print('https://raw.githubusercontent.com/gift4bd/yt-to-m3u/main/assets/moose_na.m3u8')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
